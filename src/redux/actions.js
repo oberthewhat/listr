7f9ff0902dabc7e982420bac549d9caf31c021a9
John Obertubbesing <oberthewhat@gmail.com>
2020-05-05 20:44:34 -0500

    working on fetch

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,13 +1,26 @@
 export const userLogIn = (username) => {
 	return {
-		type:"LOGIN",
+		type: "LOGIN",
 		value: username
 	}
 }
 
 export const newUser = (text) => {
 	return {
-		type:"CREATE_USER",
+		type: "CREATE_USER",
 		text
 	}
 }
+
+export const getPlaces = (type) => {
+	return (dispatch) => {
+		fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?input=A${type}&inputtype=textquery&fields=formatted_address,name&locationbias=circle:2000@ipbias&key=AIzaSyB8yc8dpOl3hsMFyctn39j3pOMferzdE64`)
+			.then(res => res.json()
+				.then(response => {
+					const action = {
+						type: 'FETCH_RESTAURANTS',
+						value: response.results
+					}
+					dispatch(action)
+				}))
+	}}
