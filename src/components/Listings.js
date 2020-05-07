4d2765b6577a2f111b769b9cc9c5b26a140dffc1
John Obertubbesing <oberthewhat@gmail.com>
2020-05-07 15:39:15 -0500

    only rendering one item

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -10,50 +10,76 @@ import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import Button from '@material-ui/core/button'
 import Box from '@material-ui/core/Box';
 
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     width: '100%',
-//   },
-//   heading: {
-//     fontSize: theme.typography.pxToRem(15),
-// 		fontWeight: theme.typography.fontWeightRegular,
-// 	},
-// 	item: {
-// 		width: '100%',
-// 		display: 'flex',
+const useStyles = makeStyles(theme => ({
+	root: {
+		width: '100%',
+	},
+	heading: {
+		fontSize: theme.typography.pxToRem(15),
+		fontWeight: theme.typography.fontWeightRegular,
+	},
+	item: {
+		width: '100%',
+		display: 'flex',
 
-// 	},
-// 	voter: {
-// 		backgroundColor: 'lightgray',
-// 		display: 'flex',
-// 	},
-// 	paper: {
-// 		width: '75%',
-// 		textAlign: 'center',
-// 	},
-// 	paperGrid: {
-// 		display: 'flex',
-// 		justifyContent: 'center'
-// 	}
-// }));
+	},
+	voter: {
+		backgroundColor: 'lightgray',
+		display: 'flex',
+	},
+	paper: {
+		width: '75%',
+		textAlign: 'center',
+	},
+	paperGrid: {
+		display: 'flex',
+		justifyContent: 'center'
+	}
+}));
 
 
 
 
 const Listings = (props) => {
-	// const classes = useStyles();
-	console.log('listings component', props.place[0])
+	const classes = useStyles();
+	const each = props.place
+	console.log('listings component', props.place)
+	return (
+		<div className={classes.root}>
+			{each.map((rest, idx) => (
+				<div className={classes.item} key={'listSection', idx}>
+					<div className={classes.voter} >
+						<Box boxShadow={2} className={classes.voter}>
+							<Button >
+								<ArrowUpwardIcon />
+							</Button>
+							<Button>
+								<ArrowDownwardIcon />
+							</Button>
+						</Box>
+					</div>
 
-  return (
-    <div className='listingsRoot'>
-			{props.place.map((rest, idx) => (
-		
-		   <div key={idx}>{rest["name"]}</div>
+					<ExpansionPanel>
+						<ExpansionPanelSummary
+							expandIcon={<ExpandMoreIcon />}
+							aria-controls="panel1a-content"
+							id="panel1a-header"
+						>
+							<Typography className={classes.heading}>{rest[idx].name}</Typography>
+						</ExpansionPanelSummary>
+						<ExpansionPanelDetails>
+							<Typography>
+								some information about the restaurant
+          </Typography>
+						</ExpansionPanelDetails>
+					</ExpansionPanel>
+				</div>
+
 
 			))}
 
-    </div>
-  );
+		</div>
+	);
 }
 
 export default Listings
