6aba9afaeca2e68cb5091cbb7c1f3fa4dacc162f
John Obertubbesing <oberthewhat@gmail.com>
2020-05-07 00:43:50 -0500

    wtf

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -20,9 +20,23 @@ export const getCords = (cords) => {
 	}
 }
 
+// zomato fucks
+
+// https://developers.zomato.com/api/v2.1/search?count=25&lat=30.059185300000003&lon=-97.8141421&radius=2000&cuisines=pizza
+
+
+
+// https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&rankby=distance&type=muse&key=AIzaSyB8yc8dpOl3hsMFyctn39j3pOMferzdE64
+
 //location bias search 
 
+
+
 // https://maps.googleapis.com/maps/api/place/textsearch/json?input=pizza&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@30.054809600000002,-97.8157568&key=AIzaSyB8yc8dpOl3hsMFyctn39j3pOMferzdE64
+
+//nearby search
+
+// https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&rankby=distance&query=pizza&key=AIzaSyB8yc8dpOl3hsMFyctn39j3pOMferzdE64
 
 // https://maps.googleapis.com/maps/api/place/textsearch/json?query=pizza&locationbias=circle:2000@ipbias&key=AIzaSyB8yc8dpOl3hsMFyctn39j3pOMferzdE64
 
@@ -33,11 +47,17 @@ export const getPlace = (lat, lon) => {
 
 	const proxyUrl = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=pizza&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@30.054809600000002,-97.8157568&key=AIzaSyB8yc8dpOl3hsMFyctn39j3pOMferzdE64`
 
+	const zomatoAPI = `https://developers.zomato.com/api/v2.1/search?q=burgers&count=20&lat=30.054809600000002&lon=-97.8157568&radius=0`
+
 	return (dispatch) => {
-		fetch(proxyUrl)
+		fetch(zomatoAPI, { 
+			headers: {
+				"user-key":"5acb8cd02eec7af3fc19b1d95b41070d"
+			}
+		})
 			.then(res => res.json()
 				.then(response => {
-					console.log('actions', response.request)
+					console.log('actions', response)
 					const action = {
 						type: 'FETCH_PLACE',
 						value: response.results,
