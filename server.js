13738c36ee2e8377fa006be164743fd1b2e39339
John Obertubbesing <oberthewhat@gmail.com>
2020-05-17 00:09:49 -0500

    placed proxy back

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const restaurantRouter = require('./backend/router/restaurants')
 const authRouter = require("./backend/router/auth");
 const app = express()
 const port = process.env.PORT || 8080;
-// var host = process.env.HOST || '127.0.0.1';
+var host = process.env.HOST || '127.0.0.1';
 
-// var cors_proxy = require('cors-anywhere');
-// cors_proxy.createServer({
-//     originWhitelist: [], // Allow all origins
-//     requireHeader: ['origin', 'x-requested-with'],
-//     removeHeaders: ['cookie', 'cookie2']
-// }).listen(port, host, function() {
-//     console.log('Running CORS Anywhere on ' + host + ':' + port);
-// });
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
