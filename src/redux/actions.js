a21f48e9155110e2941ec21ef7b3657204933700
John Obertubbesing <oberthewhat@gmail.com>
2020-05-20 01:16:49 -0500

    changed search results from 20 to 25

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -34,7 +34,7 @@ export const getPlace = (lat, lon, type) => {
 
 	const apiKey = "p7pqhAhOBOUl2TTVv5dv3wG5aJeF5B88Omtb6vTw_yQv9qT75lLeB_0KnVsG__KXNTBte05YWGixZixSprR1XpLuL4QCuVMhHZ4ydate1TEBdHCpSluwzkBb2ja6XnYx"
 	const proxy = "https://cors-anywhere.herokuapp.com/"
-	const yelp = `https://api.yelp.com/v3/businesses/search?term=${type}&latitude=${lat}&longitude=${lon}&limit=20&categories=restaurant`
+	const yelp = `https://api.yelp.com/v3/businesses/search?term=${type}&latitude=${lat}&longitude=${lon}&limit=25&categories=restaurant`
 
 	return (dispatch) => {
 		fetch(proxy + yelp, { 
